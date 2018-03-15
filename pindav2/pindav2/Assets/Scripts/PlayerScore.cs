86e3f4d25b83d07af6199c9def4d36c33a27e2c5
muiz1600 <muiz1600@student.nhl.nl>
2018-03-15 12:15:32 +0100

    Score + Timer in pindav2

---
+++ pindav2/pindav2/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+
+    private float timeLeft = 60f;
+    public int score = 0;
+    public GameObject timeLeftUI;
+    public GameObject scoreUI;
+
+    void Update()
+    {
+        Debug.Log(timeLeft);
+        timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time left: " + (int)timeLeft);
+        scoreUI.gameObject.GetComponent<Text>().text = ("Score: " + score);
+        if (timeLeft < 0.1f)
+        {
+            SceneManager.LoadScene("prototype1"); //reset level
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    { //collison with level end
+        if (trig.gameObject.name == "EndLevel")
+        {
+            CountScore();
+        }
+        if (trig.gameObject.tag == "Coin")
+        {
+            score = score + 50;
+            Destroy(trig.gameObject);
+        }
+
+    }
+
+    void CountScore()
+    {
+        score = score + (int)(timeLeft * 10);
+        Debug.Log(score);
+    }
+}
